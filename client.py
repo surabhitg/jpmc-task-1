95ce672ea59f9496a04d5c63d032cc38227cfc05
surabhitg <73701169+surabhitg@users.noreply.github.com>
2023-01-21 18:36:48 +0530

    Update client.py

--- client.py
+++ client.py
@@ -27,6 +27,7 @@ QUERY = "http://localhost:8080/query?id={}"
 
 # 500 server request
 N = 500
+previous_prices = {'ABC': [], 'DEF': []}
 
 
 def getDataPoint(quote):
@@ -35,14 +36,18 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
+
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if(price_b==0):
+        return
+        
+    return price_a/price_b
 
 
 # Main
@@ -55,5 +60,10 @@ if __name__ == "__main__":
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            
+             if len(previous_prices['ABC']) > 1 and len(previous_prices['DEF']) > 1:
+            stock_correlation = numpy.corrcoef(previous_prices['ABC'], previous_prices['DEF'])
+         print("Correlation between %s and %s is %s" % ('ABC', 'DEF', stock_correlation[0][1]))
+            
 
-        print("Ratio %s" % getRatio(price, price))
+ 
